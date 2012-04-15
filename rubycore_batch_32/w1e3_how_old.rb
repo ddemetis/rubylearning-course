407e32a48f508c6e9f868bebd67592094f129613
Demetris Demetriou <ddemetis@nohideout.net>
2012-04-16 02:23:29 +0300

    na

--- rubycore_batch_32/w1e3_how_old.rb
+++ rubycore_batch_32/w1e3_how_old.rb
@@ -1,6 +1,12 @@
 age_seconds = 979000000
 
-your_age = [age_seconds.to_f, 60, 60 , 24, 365].inject("/")
+seconds = 60 
+minutes = 60
+hours = 24
+days = 365
+
+
+your_age = [age_seconds.to_f, seconds, minutes , hours, days].inject("/")
 
 puts "%.2f" % your_age
 
--- rubycore_batch_32/w1e4_minutes_in_year.rb
+++ rubycore_batch_32/w1e4_minutes_in_year.rb
@@ -1,1 +1,6 @@
-puts 365 * 24 * 60
+days = 365
+hours = 24
+minutes = 60
+totalminutes = days * hours * minutes
+
+puts totalminutes.to_s.gsub(/(\d)(?=\d{3}+(\.\d*)?$)/, '\1,')
