c0bce0b60c2a3b9c8d03f16726ead27f15119590
Demetris Demetriou <ddemetis@nohideout.net>
2012-04-18 20:39:38 +0300

    revised w1c3_text_formatting.rb to solve problems

--- rubycore_batch_32/w1c3_text_formatting.rb
+++ rubycore_batch_32/w1c3_text_formatting.rb
@@ -1,37 +1,4 @@
-def multiplication_table (ntimes, heading = '', decorate = false)
-	times_table = []
-
-# find size of max integer and do analog formatting for sprintf
-	digits_for_formatting = (ntimes * ntimes).to_s.size + 1
-	formatting = "%" + digits_for_formatting.to_s + "d"
-	table_width = ntimes * digits_for_formatting + 2
-
-# add heading if true and decorate
-	if heading.length > 1
-		times_table << "Time Table to #{ntimes}".center(table_width) + "\n"
-	end
-	times_table << borderline(table_width, "=") if decorate
-
-
-# build the table
-	1.upto(ntimes ) do |row|
-		rows = []
-		1.upto(ntimes) do |table_field|
-			rows[table_field] = sprintf formatting % (row * table_field )
-		end
-  	rows << "\n"
-		rows.flatten!
-		rows.to_s
-		times_table << rows
-	end
-
-# decorate and uniform spacing around table
-	times_table << borderline(table_width, "=") if decorate	
-	uniform_spacing(times_table.flatten.join, table_width)
-
-end
-
-
+#creates a line of ntimes decoracation_symbol
 def borderline(ntimes, decoration_symbol)
 	borderline = ""
 	1.upto(ntimes) do
@@ -40,21 +7,88 @@ def borderline(ntimes, decoration_symbol)
 	return borderline + "\n"
 end
 
-def uniform_spacing(tablestring,ntimes)
+#adds surrounding empty ntimes spaces to source string
+def uniform_spacing(sourcestring,ntimes)
 	newstring = ""
 	spacing = "     "
-	newstring << spacing + borderline(ntimes, " ") + spacing + "\n"
-	tablestring.each_line do |line|
-		newstring << spacing + line + spacing + "\n"
+	newstring << spacing + borderline(ntimes, " ") + spacing 
+	sourcestring.each_line do |line|
+		newstring << spacing + line + spacing
 	end	
-	newstring << spacing + borderline(ntimes, " ") + spacing + "\n"
+	newstring << spacing + borderline(ntimes, " ") + spacing 
 	return newstring
 end
 
+#creates a multiplication table of ntimes, with optional heading and decoration
+def multiplication_table (ntimes, heading = '', decorate = false)
+
+	if ntimes == 0 
+		return "0 does not have multiplications. Please enter a value of 1 and above"
+	end
+
+	times_table = []
+	table_width = 0
+	temporary_times_table = []
+
+
+
+	# build the table
+	1.upto(ntimes ) do |row|
+		rows = []
+
+		1.upto(ntimes) do |table_field|
+
+		digits_size = (table_field * ntimes).to_s.size + 1
+
+		# formatting based on every column's max width
+		formatting = "%" + digits_size.to_s + "d"   
+
+		# sum up all rows width
+		table_width += digits_size if row == 1  #control line. only count the first line widths
+
+			rows[table_field] = sprintf formatting % (row * table_field )
+		end
+  	rows << "\n"
+		rows.flatten!
+		rows.to_s
+		temporary_times_table << rows
+	end
+
+
+# create the table
+	table_width += 1
+	# add heading if true and decorate
+	if heading.length > 1
+		times_table << heading.center(table_width) + "\n"
+	end
+
+	#add top borderline
+	times_table << borderline(table_width, "=") if decorate
+
+	#add main table
+	times_table << temporary_times_table
+
+	#add bottom borderline
+	times_table << borderline(table_width, "=") if decorate	
+
+	#add decoration and uniform spacing around table
+	uniform_spacing(times_table.flatten.join, table_width)
+
+end
+
+
+#main program that runs all methods above
 
 table1 = multiplication_table 9, 'Times Table to 9', true
-table2 = multiplication_table 20
-
+table2 = multiplication_table 20, '', true
 puts table1
 puts
 puts table2
+puts
+puts multiplication_table 2, 'times to 2', true
+puts
+puts multiplication_table 1, 'times to 1', true
+
+puts
+puts multiplication_table 0, 'times to 0', true
+
