fdefa2584e65e1ac6871c1a5c4dfd7b86226d27a
Demetris Demetriou <ddemetis@nohideout.net>
2012-04-19 13:35:35 +0300

    revised code of w1c2_convert.rb

--- rubycore_batch_32/w1c2_convert.rb
+++ rubycore_batch_32/w1c2_convert.rb
@@ -3,20 +3,44 @@
 # menu caption for target conversion, block of code (lambda) to execute upon selecting menu item
 
 def choices
-  [ 
-							[0, "Type 0", "Exit", lambda{|degrees| degrees}],					
-							[1, "Fahrenheit", "Celsius", lambda{|degrees| (degrees - 32) * 5 / 9}],
-							[2, "Celsius", "Fahrenheit", lambda{|degrees| degrees * 9 / 5 + 32}],
-							[3, "Celsius", "Kelvin", lambda{|degrees| degrees + 273.15}],
-							[4, "Kelvin", "Celsius", lambda{|degrees|	degrees - 273.15}],
-							[5, "Rankine", "Kelvin", lambda{|degrees|   degrees * 5 / 9}],
-							[6, "Delisle", "Kelvin", lambda{|degrees|	(-degrees * 2 / 3) + 373.15}],
-							[7, "Newton", "Kelvin", lambda{|degrees| 	degrees * 100 / 33 + 273.15}],
-							[8, "Reaumur", "Kelvin", lambda{|degrees| 	degrees * 5 / 4 + 273.15}],
-							[9, "Romer", "Kelvin", lambda{|degrees|	(degrees - 7.5) * 40 / 21 + 273.15}]
-  ]
+ [ 
+	[0, "Type 0", "Exit", 
+		lambda{|degrees| degrees}
+	],					
+	[1, "Fahrenheit", "Celsius",
+		Proc.new {|degrees| (degrees - 32) * 5 / 9}
+	],
+	[2, "Celsius", "Fahrenheit",
+		Proc.new {|degrees| degrees * 9 / 5 + 32}
+	],
+	[3, "Celsius", "Kelvin",
+		Proc.new {|degrees| degrees + 273.15}
+	],
+	[4, "Kelvin", "Celsius",
+		Proc.new {|degrees|	degrees - 273.15}
+	],
+	[5, "Rankine", "Kelvin",
+		Proc.new {|degrees|   degrees * 5 / 9}
+	],
+	[6, "Delisle", "Kelvin",
+		Proc.new {|degrees|	(-degrees * 2 / 3) + 373.15}
+	],
+	[7, "Newton", "Kelvin",
+		Proc.new {|degrees| 	degrees * 100 / 33 + 273.15}
+	],
+	[8, "Reaumur", "Kelvin",
+		Proc.new {|degrees| 	degrees * 5 / 4 + 273.15}
+	],
+	[9, "Romer", "Kelvin",
+		Proc.new {|degrees|	(degrees - 7.5) * 40 / 21 + 273.15}
+	]
+ ]
 end
 
+# conversion method that looks up the array above choices and fills up values and call block of code
+def conversion_result(input,degrees)
+	choices[input][3].call(degrees)
+end
 
 #display the menu and get us back the selection of user
 def show_menu
@@ -27,29 +51,17 @@ def show_menu
 	gets.chomp
 end
 
-
-# conversion method that looks up the array above choices and fills up values and call block of code
-def call_conversion(selection)
-	source = choices[selection][1].downcase
-	target = choices[selection][2].downcase
-
-	puts "Please enter degrees in #{source}:"
-	input = gets.chomp.to_f
-	converted_result = choices[selection][3].call(input).round(2)
-
-	puts "#{input} degrees of #{source} are %.2f degrees of #{target}" % 	converted_result
-end
-
-# clear the screen but first
-# check for os type. a simpler but not so safe approach (f.ex java platforms)
-
+# clear the screen but first check for os type.
 def clear_screen
-	if RUBY_PLATFORM.include?("win32")
-		system("cls")
-	else
-		system("clear")
+	platforms = [
+								[/win32|win64/, "cls"],
+								[/linux|bsd|darwin/, "clear"]
+							]
+	platforms.each do |os|
+		system(os[1]) if RUBY_PLATFORM.match os[0]
 	end
 end
+
 
 # the main program that calls all of the above methods
 
@@ -57,15 +69,17 @@ clear_screen
 
 while (input = show_menu) != "0"
 	input = input.to_i
-	menu_choices = choices
-	first = menu_choices.first
-	last = menu_choices.last
-	if input > first[0] and input <= last[0]  then
-			call_conversion(input)
+	menu = choices
+	menu_indexes = menu.first[0]..menu.last[0]
+
+	if menu_indexes.include?(input)
+		puts "Please enter degrees in #{menu[input][1]}:"
+		degrees = gets.to_f
+		puts "#{degrees} degrees of #{menu[input][1]} are %.2f degrees of #{menu[input][2]}" % 	conversion_result(input,degrees).round(2)
 	else
-			puts "Unrecognized option. Please select a valid option."		
+		puts "Unrecognized option. Please select a valid option."		
 	end
-	puts  # empty line
+	puts
 end
 
 puts "Goodbye!"
