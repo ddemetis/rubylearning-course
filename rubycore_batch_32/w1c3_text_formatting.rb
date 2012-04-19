98637e9ae3434636197e7e55ad866e0d6e8e98a5
Demetris Demetriou <ddemetis@nohideout.net>
2012-04-19 19:18:16 +0300

    na

--- rubycore_batch_32/w1c3_text_formatting.rb
+++ rubycore_batch_32/w1c3_text_formatting.rb
@@ -3,50 +3,42 @@
 def uniform_spacing(sourcestring,ntimes)
 	newstring = ""
 	spacing = "     "
-	newstring << spacing + (" " * ntimes + "\n") + spacing 
+	newstring << spacing + (" " * ntimes) + spacing  + "\n"
 	sourcestring.each_line do |line|
-		newstring << spacing + line + spacing
+		newstring << spacing + line.chomp  + spacing + "\n"
 	end	
-	newstring << spacing + (" " * ntimes + "\n") + spacing 
+	newstring << spacing + (" " * ntimes) + spacing  + "\n"
 	return newstring
 end
 
 #creates a multiplication table of ntimes, with optional heading and decoration
 def multiplication_table (ntimes, heading = '', decorate = false)
 
-	if ntimes == 0 
-		return "0 does not have multiplications. Please enter a value of 1 and above"
-	end
-
 	times_table = []
 	table_width = 0
 	temporary_times_table = []
 
-
-
 	# build the table
-	1.upto(ntimes ) do |row|
+	ntimes == 0 ? startat = 0 : startat = 1
+	startat.upto(ntimes) do |row|
 		rows = []
-			1.upto(ntimes) do |table_field|
-				digits_size = (table_field * ntimes).to_s.size + 1
+			startat.upto(ntimes) do |column|
+				digits_size = (column * ntimes).to_s.size + 1
 
 				# formatting based on every column's max width
 				formatting = "%" + digits_size.to_s + "d"   
 
 				# sum up all rows width
-				table_width += digits_size if row == 1  #control line. only count the first line widths
-
-				rows[table_field] = sprintf formatting % (row * table_field )
+				table_width += digits_size if row < 2  #control line. only count the first line widths and zero line too
+				rows[column] = sprintf formatting % (row * column )		  	
 			end
-  	rows << "\n"
-		rows.flatten!
-		rows.to_s
-		temporary_times_table << rows
+		rows << "\n"
+		temporary_times_table << rows.join
 	end
-
 
 # create the table
 	table_width += 1
+
 	# add heading if true and decorate
 	if heading.length > 1
 		times_table << heading.center(table_width) + "\n"
@@ -62,22 +54,22 @@ def multiplication_table (ntimes, heading = '', decorate = false)
 	times_table << "=" * table_width + "\n" if decorate
 
 	#add decoration and uniform spacing around table
-	uniform_spacing(times_table.flatten.join, table_width)
+	uniform_spacing(times_table.join, table_width)
 end
 
 
 #main program that runs all methods above
 
 table1 = multiplication_table 9, 'Times Table to 9', true
-table2 = multiplication_table 20, '', true
+table2 = multiplication_table 20
 puts table1
 puts
 puts table2
 puts
-puts multiplication_table 2, 'times to 2', true
+puts multiplication_table 2, 'Times to 2', true
 puts
-puts multiplication_table 1, 'times to 1', true
+puts multiplication_table 1, 'Times to 1', true
 
 puts
-puts multiplication_table 0, 'times to 0', true
+puts multiplication_table 0, 'Times to 0', true
 
