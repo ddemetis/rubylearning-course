b864e3688f1821f51c56c83fadacaac91e5f0159
Demetris Demetriou <ddemetis@nohideout.net>
2012-04-25 23:08:51 +0300

    na

---
+++ rubycore_batch_32/w2c3_column_string_alignment.rb
@@ -0,0 +1,13 @@
+class String
+	#prefix the string and number it
+	def formatlines(left_gutter = "Line ")
+		all_lines = split("\n")
+		max_width = all_lines.size.to_s.size
+	 	all_lines.each.with_index do |line,index|
+			string_format = "#{left_gutter} %0#{max_width}d: " % (index + 1).to_s
+			all_lines[index] =  string_format + line
+		end
+	end
+end
+
+puts ("test me one thousand times\n" * 1000).formatlines
